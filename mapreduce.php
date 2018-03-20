fb3a787a9574d77c53aebde92655d2292d252e0b
avanish228 <avanishsingh228@gmail.com>
2018-03-20 17:08:37 +0530

    Add files via upload

---
+++ mapreduce.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Mapreduce</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="color/default.css" rel="stylesheet">
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<!-- =======================================================
+    Theme Name: Maxim
+    Theme URL: https://bootstrapmade.com/maxim-free-onepage-bootstrap-theme/
+    Author: BootstrapMade.com
+    Author URL: https://bootstrapmade.com
+	======================================================= -->
+</head>
+
+<body>
+	<!-- navbar -->
+	<div class="navbar-wrapper">
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<!-- Responsive navbar -->
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+				</a>
+					<h1 class="brand"><a href="home.html">Localhost</a></h1>
+					<!-- navigation -->
+					<nav class="pull-right nav-collapse collapse">
+						<ul id="menu-main" class="nav">
+							<li><a title="team" href="home.html#about">About</a></li>
+							<li><a title="services" href="home.html#services">Services</a></li>
+							<li><a title="blog" href="saas.html">SAAS</a></li>
+							<li><a title="contact" href="paas.html">PAAS</a></li>
+                            <li><a title="contact" href="logout.php">LOGOUT</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+	<section id="services" class="section green">
+        <form action="" method="post">
+		<div class="container">
+			<h4>Upload your Mapper/Reducer</h4>
+			<!-- Four columns -->
+			<div class="row">
+				<div class="span3 animated-fast flyIn">
+					
+				</div>
+				<div class="span3 animated flyIn">
+					<div class="button">
+						<input type="file" name="mapper" class="button"  value="Mapper" />
+						
+					</div>
+				</div>
+				<div class="span3 animated-fast flyIn">
+					<div class="button">
+						<input type="file" name="reducer" class="button" value="Reducer" />
+						
+						
+					</div>
+				</div>
+				
+			</div>
+			<div class="row">
+				<div class="span3 animated-slow flyIn">
+					<center><input type="submit" class="button" value="Submit" /></center>
+				</div>
+			</div>
+		</div>
+        </form>
+	</section>
+	<!-- end section: services -->
+	<!-- section: works -->
+	
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="span6 offset3">
+					<p class="copyright">
+						&copy; Localhost. All rights reserved.
+					</p>
+				</div>
+			</div>
+		</div>
+		<!-- ./container -->
+	</footer>
+	<a href="#" class="scrollup"><i class="icon-angle-up icon-square icon-bgdark icon-2x"></i></a>
+	<script>
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.nav.js"></script>
+	<script src="js/jquery.localScroll.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/jquery.prettyPhoto.js"></script>
+	<script src="js/isotope.js"></script>
+	<script src="js/jquery.flexslider.js"></script>
+	<script src="js/inview.js"></script>
+	<script src="js/animate.js"></script>
+	<script src="js/custom.js"></script>
+	<script src="contactform/contactform.js"></script>
+
+</body>
+
+</html>
